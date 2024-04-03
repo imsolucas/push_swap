f2dccc2b934a5c09753d768f56eb1648831117ff
Lucas <denglucasyijin@gmail.com>
2024-04-03 23:29:59 +0800

    fixed atol and done push_swap

--- srcs/check/checks3.c
+++ srcs/check/checks3.c
@@ -6,7 +6,7 @@
 /*   By: geibo <geibo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 17:48:36 by djin              #+#    #+#             */
-/*   Updated: 2024/04/03 23:17:48 by geibo            ###   ########.fr       */
+/*   Updated: 2024/04/03 23:27:36 by geibo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,8 @@ int	ft_atol(char *str, t_stack *stack_a, int argc, char **argv)
 		num = (num * 10) + (str[i] - '0');
 		i++;
 	}
-	num *= sign;
-	if ((num * sign) >= 2147483648 || (num * sign) <= -2147483649)
+	if ((num * sign) > 2147483647 || (num * sign) < -2147483648)
 		error_free_exit("Error\n", stack_a, argc, argv);
+	num *= sign;
 	return ((int)num);
 }
